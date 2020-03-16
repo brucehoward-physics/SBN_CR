66689fb8b94ebdf05e8c334102591b003bd50d36
Bruce Howard <howard@fnal.gov>
2020-03-16 16:48:00 -0500

    Made a shell script to run that picks up the absolute path for the desktop icons for a user and makes the desktop files in a shell script. This seems to work.

--- CR-From-Home/README.txt
+++ CR-From-Home/README.txt
@@ -6,11 +6,14 @@ This DOES NOT cover iFix. You need to follow the Remote Desktop instructions for
 
 If you have a Linux distribution, you can run the programs by double-clicking the ".desktop" item corresponding to it. Allow it to run, and then you should see it becomes an icon that launches the needed terminal :)
 
+To set up the desktop icons, run the setup_icons.sh file from this directory. It will replace the Exec and Icon lines with the proper working area for your system, by doing a PWD and using sed to replace the lines...
+
 If you have a Mac OS, then you should instead run the program from the scripts subdirectory. They are executable, so if you cd into the directory you can do, for example,
 
    ./vncDAQ.sh
 
 to tunnel to the DAQ machine. Then go to a web browser and go to the necessary address.
+
 
 
 NOTES:
---
+++ CR-From-Home/setup_icons.sh
@@ -0,0 +1,38 @@
+myDir=`echo $PWD`
+
+rm -f vncDAQ.desktop
+rm -f vncDCS.desktop
+rm -f connectGrafana.desktop
+
+echo "[Desktop Entry]" >> vncDAQ.desktop
+echo "version=1.0" >> vncDAQ.desktop
+echo "Exec="${myDir}"/scripts/setup_tunnel" >> vncDAQ.desktop
+echo "Name=vnc_DAQ" >> vncDAQ.desktop
+echo 'Comment="Sets up tunnel to DAQ VNC"' >> vncDAQ.desktop
+echo "Icon="${myDir}"/icons/vncDAQ.png" >> vncDAQ.desktop
+echo "Terminal=true"  >> vncDAQ.desktop
+echo "Type=Application" >> vncDAQ.desktop
+
+chmod u+x vncDAQ.desktop
+
+echo "[Desktop Entry]" >> vncDCS.desktop
+echo "version=1.0" >> vncDCS.desktop
+echo "Exec="${myDir}"/scripts/setup_tunnel_dcs" >> vncDCS.desktop
+echo "Name=vnc_DCS" >> vncDCS.desktop
+echo 'Comment="Sets up tunnel to DCS VNC"' >> vncDCS.desktop
+echo "Icon="${myDir}"/icons/vncDCS.png" >> vncDCS.desktop
+echo "Terminal=true"  >> vncDCS.desktop
+echo "Type=Application" >> vncDCS.desktop
+
+chmod u+x vncDCS.desktop
+
+echo "[Desktop Entry]" >> connectGrafana.desktop
+echo "version=1.0" >> connectGrafana.desktop
+echo "Exec="${myDir}"/scripts/connect_grafana" >> connectGrafana.desktop
+echo "Name=connect_grafana" >> connectGrafana.desktop
+echo 'Comment="Sets up tunnel to Grafana"' >> connectGrafana.desktop
+echo "Icon="${myDir}"/icons/connect_grafana.png" >> connectGrafana.desktop
+echo "Terminal=true"  >> connectGrafana.desktop
+echo "Type=Application" >> connectGrafana.desktop
+
+chmod u+x connectGrafana.desktop
