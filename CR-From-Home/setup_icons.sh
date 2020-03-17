3abd9c39a2c7321503ad52ddc0215847737dc3c8
Justin Mueller <Justin.Mueller@colostate.edu>
2020-03-17 10:37:57 -0500

    Fixed capitalization for default desktop file key 'version'

--- CR-From-Home/setup_icons.sh
+++ CR-From-Home/setup_icons.sh
@@ -5,7 +5,7 @@ rm -f vncDCS.desktop
 rm -f connectGrafana.desktop
 
 echo "[Desktop Entry]" >> vncDAQ.desktop
-echo "version=1.0" >> vncDAQ.desktop
+echo "Version=1.0" >> vncDAQ.desktop
 echo "Exec="${myDir}"/scripts/setup_tunnel" >> vncDAQ.desktop
 echo "Name=vnc_DAQ" >> vncDAQ.desktop
 echo 'Comment="Sets up tunnel to DAQ VNC"' >> vncDAQ.desktop
@@ -16,7 +16,7 @@ echo "Type=Application" >> vncDAQ.desktop
 chmod u+x vncDAQ.desktop
 
 echo "[Desktop Entry]" >> vncDCS.desktop
-echo "version=1.0" >> vncDCS.desktop
+echo "Version=1.0" >> vncDCS.desktop
 echo "Exec="${myDir}"/scripts/setup_tunnel_dcs" >> vncDCS.desktop
 echo "Name=vnc_DCS" >> vncDCS.desktop
 echo 'Comment="Sets up tunnel to DCS VNC"' >> vncDCS.desktop
@@ -27,7 +27,7 @@ echo "Type=Application" >> vncDCS.desktop
 chmod u+x vncDCS.desktop
 
 echo "[Desktop Entry]" >> connectGrafana.desktop
-echo "version=1.0" >> connectGrafana.desktop
+echo "Version=1.0" >> connectGrafana.desktop
 echo "Exec="${myDir}"/scripts/connect_grafana" >> connectGrafana.desktop
 echo "Name=connect_grafana" >> connectGrafana.desktop
 echo 'Comment="Sets up tunnel to Grafana"' >> connectGrafana.desktop
